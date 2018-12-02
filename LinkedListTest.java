ea848241370bf076d79b23db86d194f7a3e8b86a
Rajan Khullar <rkhullar03@gmail.com>
2018-12-02 10:38:04 -0500

    adding tests for adding at head and tail

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
@@ -26,7 +27,7 @@ public class LinkedListTest
         }
     }
 
-    private static LinkedList build_list(LinkedListNode head, LinkedListNode tail, int size) throws NoSuchFieldException, IllegalAccessException
+    private static HashMap<String, Field> load_list_fields() throws NoSuchFieldException
     {
         final String[] field_names = {"head", "tail", "size"};
         HashMap<String, Field> fields = new HashMap<>();
@@ -38,17 +39,55 @@ public class LinkedListTest
             fields.put(name, field);
         }
 
+        return fields;
+    }
+
+    private static LinkedList build_list(LinkedListNode head, LinkedListNode tail, int size) throws NoSuchFieldException, IllegalAccessException
+    {
+        HashMap<String, Field> fields = load_list_fields();
         LinkedList list = new LinkedList();
         fields.get("head").set(list, head);
         fields.get("tail").set(list, tail);
         fields.get("size").set(list, size);
-
         return list;
     }
 
     private static LinkedList build_list(LinkedListNode nodes[]) throws NoSuchFieldException, IllegalAccessException
     {
-        return build_list(nodes[0], nodes[nodes.length-1], nodes.length);
+        if (nodes.length > 0)
+            return build_list(nodes[0], nodes[nodes.length-1], nodes.length);
+        else
+            return build_list(null, null, 0);
+    }
+
+    private static <T> LinkedList build_list(T...data) throws NoSuchFieldException, IllegalAccessException
+    {
+        LinkedListNode nodes[] = build_node_array(data);
+        link_node_array(nodes);
+        return build_list(nodes);
+    }
+
+    private static int list_size(LinkedListNode start)
+    {
+        int count = 0;
+        for(; start != null; count++, start = start.next);
+        return count;
+    }
+
+    private static Object[] list_to_array(LinkedListNode start, int size)
+    {
+        Object array[] = new Object[size];
+        for(int count = 0; start != null; count++, start=start.next)
+            array[count] = start.getData();
+        return array;
+    }
+
+    private static Object[] list_to_array(LinkedList list) throws NoSuchFieldException, IllegalAccessException
+    {
+        HashMap<String, Field> fields = load_list_fields();
+        LinkedListNode head = (LinkedListNode) fields.get("head").get(list);
+        int size = list_size(head);
+        return list_to_array(head, size);
     }
 
     @Test
@@ -87,10 +126,31 @@ public class LinkedListTest
     @Test
     public void test_list_to_string() throws NoSuchFieldException, IllegalAccessException
     {
-        LinkedListNode nodes[] = build_node_array('a', 'b', 'c');
-        link_node_array(nodes);
-        LinkedList list = build_list(nodes);
+        LinkedList list = build_list('a', 'b', 'c');
         assertEquals("[a, b, c]", list.toString());
+    }
+
+    @Test
+    public void test_list_add_head() throws NoSuchFieldException, IllegalAccessException
+    {
+        LinkedList list = build_list();
+        list.addHead('a');
+        list.addHead('b');
+        Character expected[] = new Character[] {'b', 'a'};
+        Object[] actual = list_to_array(list);
+        //assertArrayEquals(expected, actual);
+        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+    }
+
+    @Test
+    public void test_list_add_tail() throws NoSuchFieldException, IllegalAccessException
+    {
+        LinkedList list = build_list();
+        list.addTail('a');
+        list.addTail('b');
+        Character expected[] = new Character[] {'a', 'b'};
+        Object[] actual = list_to_array(list);
+        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
     }
 
 }
